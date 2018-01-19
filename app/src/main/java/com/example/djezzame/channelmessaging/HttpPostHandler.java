f0708f03f3b8498e4c0f4fa4721264264089bc36
djezzame <mehdi.djezzar@etu.univ-savoie.fr>
2018-01-19 11:41:03 +0100

    ok

--- app/src/main/java/com/example/djezzame/channelmessaging/HttpPostHandler.java
+++ app/src/main/java/com/example/djezzame/channelmessaging/HttpPostHandler.java
@@ -21,64 +21,64 @@ import javax.net.ssl.HttpsURLConnection;
  * Created by djezzame on 19/01/2018.
  */
 public class HttpPostHandler extends AsyncTask<PostRequest,Void,String> {
-private ArrayList<OnDownloadListener> listeners = new ArrayList<OnDownloadListener> ();
+    private ArrayList<OnDownloadListener> listeners = new ArrayList<OnDownloadListener>();
 
-private void addOnDownloadListener(){
+    private void addOnDownloadListener() {
 
-        }
+    }
 
-public String performPostCall(String requestURL, HashMap<String, String>
-postDataParams) {
+    public String performPostCall(String requestURL, HashMap<String, String>
+            postDataParams) {
         URL url;
         String response = "";
         try {
-        url = new URL(requestURL);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setReadTimeout(15000);
-        conn.setConnectTimeout(15000);
-        conn.setRequestMethod("POST");
-        conn.setDoInput(true);
-        conn.setDoOutput(true);
-        OutputStream os = conn.getOutputStream();
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-        writer.write(getPostDataString(postDataParams));
-        writer.flush();
-        writer.close();
-        os.close();
-        int responseCode=conn.getResponseCode();
-        if (responseCode == HttpsURLConnection.HTTP_OK) {
-        String line;
-        BufferedReader br=new BufferedReader(new
-                InputStreamReader(conn.getInputStream()));
-        while ((line=br.readLine()) != null) {
-        response+=line;
-        }
-        }
-        else {
-        response="";
-        }
+            url = new URL(requestURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(15000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+            OutputStream os = conn.getOutputStream();
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+            writer.write(getPostDataString(postDataParams));
+            writer.flush();
+            writer.close();
+            os.close();
+            int responseCode = conn.getResponseCode();
+            if (responseCode == HttpsURLConnection.HTTP_OK) {
+                String line;
+                BufferedReader br = new BufferedReader(new
+                        InputStreamReader(conn.getInputStream()));
+                while ((line = br.readLine()) != null) {
+                    response += line;
+                }
+            } else {
+                response = "";
+            }
         } catch (Exception e) {
-        e.printStackTrace();
+            e.printStackTrace();
         }
         return response;
-        }
+    }
 
-@Override
-protected String doInBackground(PostRequest... params) {
+    @Override
+    protected String doInBackground(PostRequest... params) {
 
         return null;
-        }
+    }
 
-private String getPostDataString(HashMap<String, String> params) throws
-        UnsupportedEncodingException {
+    private String getPostDataString(HashMap<String, String> params) throws
+            UnsupportedEncodingException {
         StringBuilder result = new StringBuilder();
         boolean first = true;
-        for(Map.Entry<String, String> entry : params.entrySet()){
-        if (first) first = false;
-        else result.append("&");
-        result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-        result.append("=");
-        result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            if (first) first = false;
+            else result.append("&");
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
         }
         return result.toString();
-        }
+    }
+}
